f52554ef6aa48c1be9d779f7df6ec993893e698a
Dan L <dan784@gmail.com>
2026-05-11 07:13:56 +0300

    small fix

--- lib/home.dart
+++ lib/home.dart
@@ -204,7 +204,7 @@ class _HomeState extends State<Home> {
                     ),
                     const SizedBox(height: 6),
                     Text(
-                      '$wordCount words',
+                      '$wordCount cards',
                       style: Theme.of(context).textTheme.bodyMedium,
                     ),
                   ],
